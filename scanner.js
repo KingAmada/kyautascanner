bf20b91baa5f61404139cb47d9dc292017217bb2
KingAmada <sarkiamada@gmail.com>
2024-02-24 22:02:57 -0800

    Update scanner.js

--- scanner.js
+++ scanner.js
@@ -46,7 +46,7 @@ window.addEventListener('DOMContentLoaded', (event) => {
     // Start the QR code scanner
     Html5Qrcode.getCameras().then(cameras => {
         if (cameras && cameras.length) {
-            let selectedCameraId = cameras[0].id; // Default to the first camera (usually the front camera on mobile devices)
+            let selectedCameraId = cameras[1].id; // Default to the first camera (usually the front camera on mobile devices)
 
             // Prefer the back camera if available
             const backCamera = cameras.find(camera => camera.label.toLowerCase().includes('back'));
